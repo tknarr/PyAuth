472fc73bcba7f767a3672e9737e261736ab120e5
Todd Knarr <tknarr@silverglass.org>
2015-12-05 00:07:50 -0800

    edit

--- PyAuthApp.py
+++ PyAuthApp.py
@@ -1,14 +1,24 @@
 # -*- coding: utf-8 -*-
 
 # .PyAuth - configuration directory
-#     auth.xml - authorization file
+#     config - configuration
+#     database.xml - authorization secrets storage
+
+import PyAuth_xrc
+import wx
+from wx import xrc
 
 class PyAuthApp(wx.App):
 
-    def OnInt(self):
+    def OnInit(self):
         # TODO Load data from storage file
 
         self.res = get_resources();
-        # TODO Load and init main frame
-
+        init_frame(self)
         return True
+
+    def init_frame(self)
+        self.frame = self.res.LoadFrame(None, 'main_frame')
+        # TODO get needed items
+        # TODO do other needed initialization
+        self.frame.Show()
