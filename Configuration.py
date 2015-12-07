dbc51f2927aca15b2c4e50f46480496fb8854db1
Todd Knarr <tknarr@silverglass.org>
2015-12-06 16:47:37 -0800

    edit

--- Configuration.py
+++ Configuration.py
@@ -4,4 +4,14 @@ import wx
 
 class Configuration:
 
+    # Config items:
+    #   Last window position
+    #   Last window width
+    #   Last pegged corner
+    #   Peg to upper-left, upper-right, lower-left, lower-right corner
+    #   Number of items shown
+    #   Last auth secrets file
+    #   Show timers
+    #   Show codes for all entries
+    
     # TODO Configuration class
