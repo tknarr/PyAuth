7b7b6ece36048fd1e0c87cd2b1923585702c0a04
Todd Knarr <tknarr@silverglass.org>
2015-12-15 00:20:50 -0800

    edit

--- PyAuth.py
+++ PyAuth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import os.path
 import wx
 from AuthFrame import AuthFrame
 import Configuration
@@ -13,7 +14,16 @@ class PyAuthApp( wx.App ):
                              style = wx.CONFIG_USE_LOCAL_FILE | wx.CONFIG_USE_SUBDIR )
         cfg.SetRecordDefaults( True )
         wx.Config.Set( cfg )
-        # TODO Make sure our config directory exists
+        # Make sure the directory for our configuration file exists
+        cfgfile = wx.FileConfig.GetLocalFileName( "pyauth.cfg", wx.CONFIG_USE_LOCAL_FILE | wx.CONFIG_USE_SUBDIR )
+        cfgdir = os.path.dirname( cfgfile )
+        if not os.path.exists( cfgdir ):
+            try:
+                os.makedirs( cfgdir )
+            except OSError as e:
+                print "Failed to create config directory " + cfgdir
+                print "Error code " + str( e.errno ) + ": " + e.strerror
+                return False
 
         # Create main frame
         wpos = Configuration.GetLastWindowPosition()
