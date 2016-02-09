35f53d8d982807a7411e0dd8912f8ae032c26f17
Todd Knarr <tknarr@silverglass.org>
2016-02-08 23:43:17 -0800

    Modify desktop template during post-install

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
+from setuptools.command.install import install as _install
+from setuptools.command.develop import develop as _develop
 # To use a consistent encoding
 from codecs import open
 from os import path
 from glob import glob
+import pkg_resources
 import pyauth
 
 here = path.abspath(path.dirname(__file__))
@@ -28,29 +31,61 @@ for s in [ 16, 24, 32, 48, 64, 128, 256 ]:
     entry = ( 'share/icons/hicolor/{0}x{0}/apps'.format( str(s) ), files )
     image_files.append( entry )
 
+
+# Post-installation script
+def _post_install( data_path, script_path ):
+    # Read in the desktop shortcut template and substitute final paths into it to create
+    # the real shortcut file
+    template = data_path + '/share/doc/' + pyauth.__program_name__ + '/PyAuth.desktop.in'
+    shortcut = data_path + '/share/doc/' + pyauth.__program_name__ + '/PyAuth.desktop'
+    with open( template, 'r' ) as tf:
+        with open( shortcut, 'w' ) as sf:
+            for line in tf:
+                l = line.format( program_name = pyauth.__program_name__,
+                                 script_path = script_path,
+                                 data_path = data_path )
+                sf.write( l )
+
+
+# Classes to add post-install behavior to standard install and develop commands
+
+class my_install( _install ):
+    def run( self ):
+        _install.run( self )
+
+        # the second parameter, [], can be replaced with a set of parameters if _post_install needs any
+        self.execute( _post_install, [ self.install_data, self.install_scripts ], msg="Running post install task" )
+
+class my_develop( _develop ):
+    def run( self ):
+        self.execute( noop, ( self.install_lib ), msg="Running develop task" )
+        _develop.run( self )
+        self.execute( _post_install, [ self.install_data, self.install_scripts ], msg="Running post develop task" )
+
+
 setup(
-    name=pyauth.__program_name__,
+    name = pyauth.__program_name__,
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version=pyauth.__version__ + pyauth.__version_tag__,
+    version = pyauth.__version__ + pyauth.__version_tag__,
 
-    description='Google Authenticator (TOTP) desktop client',
-    long_description=long_description,
+    description = 'Google Authenticator (TOTP) desktop client',
+    long_description = long_description,
 
     # The project's main homepage.
-    url='https://github.com/tknarr/PyAuth',
+    url = 'https://github.com/tknarr/PyAuth',
 
     # Author details
-    author='Todd Knarr',
-    author_email='tknarr@silverglass.org',
+    author = 'Todd Knarr',
+    author_email = 'tknarr@silverglass.org',
 
     # Choose your license
-    license='GPLv3+',
+    license = 'GPLv3+',
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[
+    classifiers = [
         # How mature is this project? Common values are
         #   3 - Alpha
         #   4 - Beta
@@ -73,24 +108,24 @@ setup(
     ],
 
     # What does your project relate to?
-    keywords='authentication totp hotp 2fa',
+    keywords = 'authentication totp hotp 2fa',
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    packages=find_packages( exclude = ['contrib', 'docs', 'tests'] ),
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[
+    install_requires = [
         'pyotp>=2.0.1'
         ],
 
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    data_files= image_files + [
+    data_files = image_files + [
         ( 'share/doc/' + pyauth.__program_name__,
           [ 'README.rst',
             'LICENSE.html',
@@ -104,9 +139,13 @@ setup(
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
-    entry_points={
+    entry_points = {
         'gui_scripts': [
             'PyAuth=pyauth.__main__:main',
         ],
     },
+    cmdclass = {
+        'install': my_install,  # override install
+        'develop': my_develop   # develop is used for pip install -e .
+        }
 )
