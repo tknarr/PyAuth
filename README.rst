698bfb3d9c5a3ad1bcc65a30885a31704fe62ffa
Todd Knarr <tknarr@silverglass.org>
2016-06-05 21:06:21 -0700

    Encryption module for new Fernet256 format.

--- README.rst
+++ README.rst
@@ -6,7 +6,9 @@ Copyright 2016 Todd T Knarr <tknarr@silverglass.org>
 This program is free software: you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free Software
 Foundation, either version 3 of the License, or (at your option) any later
-version.
+version. The Fernet AES256 implementation (fernet256.py) is dual licensed
+under the terms of the Apache License version 2.0 and the BSD License as
+noted in the source file.
 
 This program is distributed in the hope that it will be useful, but WITHOUT
 ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
--- pyauth/Encryption.py
+++ pyauth/Encryption.py
@@ -3,51 +3,114 @@
 
 import string
 import base64
-from Crypto.Protocol.KDF import PBKDF2
-from Crypto.Cipher import AES
-from Crypto.Hash import SHA256, HMAC
-from Crypto import Random
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.backends import default_backend
+from fernet256 import Fernet256
 
-class AES_HMAC:
+class Fernet_256:
     """
     Encryption for the authentication store.
 
-    The token follows the general format of Fernet (https://github.com/fernet/spec/blob/master/Spec.md)
-    except using AES256 instead of AES128 for encryption.
+    The token follows the format of Fernet (https://github.com/fernet/spec/blob/master/Spec.md)
+    using AES256 instead of AES128 for encryption.
     """
 
-    def __init__( self, password ):
-        self.algorithm = 'AES256-HMAC'
-        self.block_size = AES.block_size
-        # TODO implement
+    def __init__( self, password = None, salt = None ):
+        self.algorithm = 'FERNET-256'
+        self.block_size = algorithms.AES.block_size / 8
+        self.storage_key = None
+        self.storage_key_salt = salt
+        self.backend = default_backend()
+        if password != None and self.storage_key_salt != None:
+            kdf = PBKDF2HMAC( algorithm = hashes.SHA256(),
+                              length = 64,
+                              salt = self.storage_key_salt,
+                              iterations = 100000,
+                              backend = self.backend )
+            self.storage_key = base64.urlsafe_b64encode( kdf.derive( password ) )
 
-    def ChangePassword( self, new_password ):
-        # TODO implement
+    def SetPassword( self, new_password, new_salt = None ):
+        """Derive a new storage key from a new password and optionally a new salt."""
+        if new_salt != None:
+            self.storage_key_salt = new_salt
+        if self.storage_key_salt == None:
+            raise ValueError( "No password salt set." )
+        kdf = PBKDF2HMAC( algorithm = hashes.SHA256(),
+                          length = 64,
+                          salt = self.storage_key_salt,
+                          iterations = 100000,
+                          backend = self.backend )
+        self.storage_key = base64.urlsafe_b64encode( kdf.derive( new_password ) )
 
-    def Encrypt( self, cleartext ):
-        # TODO implement
+    def Encrypt( self, secret ):
+        """Encrypt a secret using the current key."""
+        if self.storage_key == None:
+            raise ValueError( "No password set." )
+        f = Fernet256( self.storage_key )
+        padder = padding.PKCS7( algorithms.AES.block_size ).padder()
+        cleartext = padder.update( secret )
+        cleartext += padder.finalize()
+        ciphertext = f.encrypt( cleartext )
+        return unicode( ciphertext )
 
     def Decrypt( self, token ):
-        # TODO implement
+        """Decrypt a secret using the current key."""
+        if self.storage_key == None:
+            raise ValueError( "No password set." )
+        f = Fernet256( self.storage_key )
+        cleartext = f.decrypt( token )
+        unpadder = padding.PKCS7( algorithms.AES.block_size ).unpadder()
+        secret = unpadder.update( cleartext )
+        secret += unpadder.finalize()
+        return unicode( secret )
 
-    # TODO implement
 
+class Old_AES:
+    """Old and insecure AES encryption."""
 
-class AES_unauthenticated:
-    """Old unauthenticated AES decryption."""
-
-    def __init__( self, password ):
+    def __init__( self, password = None, salt = None ):
         self.algorithm = 'AES'
-        self.block_size = AES.block_size
-        # TODO implement
+        self.block_size = algorithms.AES.block_size / 8
+        self.storage_key = None
+        self.storage_key_salt = salt
+        self.backend = default_backend()
+        if password != None and self.storage_key_salt != None:
+            kdf = PBKDF2HMAC( algorithm = hashes.SHA1(),
+                              length = self.block_size,
+                              salt = self.storage_key_salt,
+                              iterations = 10000,
+                              backend = self.backend )
+            self.storage_key = base64.b64encode( kdf.derive( password ) )
+
+    def SetPassword( self, new_password, new_salt = None ):
+        """Derive a new storage key from a new password and optionally a new salt."""
+        if new_salt != None:
+            self.storage_key_salt = new_salt
+        if self.storage_key_salt == None:
+            raise ValueError( "No password salt set." )
+        kdf = PBKDF2HMAC( algorithm = hashes.SHA1(),
+                          length = 16,
+                          salt = self.storage_key_salt,
+                          iterations = 10000,
+                          backend = self.backend )
+        self.storage_key = kdf.derive( password )
+
+    def Encrypt( self, cleartext ):
+        """Encryption is no longer supported."""
+        raise NotImplementedError( "Encryption is not supported by the legacy AES algorithm." )
 
     def Decrypt( self, ciphertext ):
-        """Decrypt a secret using the current password."""
+        """Decrypt a secret using the current key."""
+        if self.storage_key == None:
+            raise ValueError( "No password set." )
         b = base64.standard_b64decode( ciphertext )
         iv = b[0:self.block_size]
-        raw_ciphertext = b[AES.block_size:]
-        cipher = AES.new( self.storage_key, AES.MODE_CBC, iv )
-        cleartext = cipher.decrypt( raw_ciphertext )
+        raw_ciphertext = b[self.block_size:]
+        cipher = Cipher( algorithms.AES( base64.b64decode( self.storage_key ) ),
+                         modes.CBC( iv ), self.backend )
+        decryptor = cipher.decryptor()
+        cleartext = decryptor.update( raw_ciphertext ) + decryptor.finalize()
         return unicode( cleartext ).rstrip()
-
-    # TODO implement
