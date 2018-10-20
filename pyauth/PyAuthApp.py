f52137d8db9af62effe82b80bcb3167f5700bab3
Todd Knarr <tknarr@silverglass.org>
2018-10-19 19:18:31 -0700

    Update copyright notices

--- pyauth/PyAuthApp.py
+++ pyauth/PyAuthApp.py
@@ -1,21 +1,24 @@
 # -*- coding: utf-8 -*-
 """Application main module."""
 
-## PyAuth - Google Authenticator desktop application
-## Copyright (C) 2016 Todd T Knarr <tknarr@silverglass.org>
-
-## This program is free software: you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation, either version 3 of the License, or
-## (at your option) any later version.
-
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-
-## You should have received a copy of the GNU General Public License
-## along with this program.  If not, see http://www.gnu.org/licenses/
+#-----
+# PyAuth
+# Copyright (C) 2018 Silverglass Technical
+# Author: Todd Knarr <tknarr@silverglass.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#-----
 
 import argparse
 import logging
